759807cf353ca590f9b8583398b69a24615c6bf0
marxsoel <sr.marxelias@gmail.com>
2023-09-04 20:05:56 -0300

    first commit

---
+++ unidade06/inicial.php
@@ -0,0 +1,15 @@
+
+
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+        <p><?php echo "Matheus Fontenelle" ?></p>
+        <p><?php echo 5*4; ?></p>
+    </body>
+</html>
