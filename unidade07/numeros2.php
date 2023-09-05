e3c1fb4bae35b56cd690b4705a1da5386e40e6b8
marxsoel <sr.marxelias@gmail.com>
2023-09-05 20:40:14 -0300

    teste de variaveis php

--- unidade07/numeros2.php
+++ unidade07/numeros2.php
@@ -1,6 +1,7 @@
 <?php
-    $salario = 1095;
-    $gasolina = 4.55;
+    $_salario = 1095;
+    $_gasolina = 4.55;
+    $_telefone = "3456-0987";
 ?>
 
 <!doctype html>
@@ -15,12 +16,20 @@
         
         <?php
             // testar se é numérica
-
-
+            echo "O $_salario e um numero?  " . is_numeric($_salario) ."</br>";
+            echo "O $_gasolina e um numero?  " . is_numeric($_gasolina) ."</br>"; 
+            echo "O $_telefone e um numero?  " . is_numeric($_telefone) ."</br>";
+            
             // testar se é inteiro
-
+            echo "O $_salario e um inteiro?  " . is_int($_salario) ."</br>";
+            echo "O $_gasolina e um inteiro?  " . is_int($_gasolina) ."</br>"; 
+            echo "O $_telefone e um inteiro?  " . is_int($_telefone) ."</br>";
 
             // testar se é float
+            echo "O $_salario e um racional?  " . is_float($_salario) ."</br>";
+            echo "O $_gasolina e um racional?  " . is_float($_gasolina) ."</br>"; 
+            echo "O $_telefone e um racional?  " . is_float($_telefone) ."</br>";
+
         ?>
         
         
